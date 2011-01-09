b0d480ed47ec9bdc3c23820b8aa2cdda71f10487
n0body <nobody@cpan.org>
2011-01-09 18:10:53 +0000

    Import of NOBODY/Exception-Simple-0.001 from CPAN.
    gitpan-cpan-distribution: Exception-Simple
    gitpan-cpan-version:      0.001
    gitpan-cpan-path:         NOBODY/Exception-Simple-0.001.tar.gz
    gitpan-cpan-author:       NOBODY
    gitpan-cpan-maturity:     released

--- lib/Exception/Simple.pm
+++ lib/Exception/Simple.pm
@@ -1,196 +1,163 @@
 package Exception::Simple;
+use strict;
+use warnings;
 
-use Moose;
+our $VERSION = '0.001';
+$VERSION = eval $VERSION;
 
-use overload '""' => \&_as_string;
+use Carp qw/croak/;
+use overload(
+    'fallback' => \&error,
+    '""' => \&error,
+);
 
-has 'error'      => (is => 'rw',isa => 'Str',required => 1);
-has 'only_error' => (is => 'rw',isa => 'Bool',default => 0);
-has 'package'    => (is => 'rw',isa => 'Str',predicate => 'has_package');
-has 'filename'   => (is => 'rw',isa => 'Str',predicate => 'has_filename');
-has 'line'       => (is => 'rw',isa => 'Int',predicate => 'has_line');
+# __public__ #
 
-our $VERSION = '0.0099_01';
-
-our $AUTHORITY = 'cpan:BERLE';
-
-sub throw {
-  my ($class,%arguments) = @_;
-
-  my ($package,$filename,$line) = caller;
-
-  $arguments{package} ||= $package;
-
-  $arguments{filename} ||= $filename;
-
-  $arguments{line} ||= $line;
-
-  my $self = $class->new (%arguments);
-
-  die $self;
+sub throw{
+    croak shift->_new( @_ );
 }
 
-sub rethrow {
-  my ($self,%arguments) = @_;
-
-  die $self;
+sub rethrow{
+    croak shift;
 }
 
-sub _as_string {
-  my ($self) = @_;
-
-  return $self->stringify;
+#error is special coz its the stringify method
+#it needs to be defined, and overridable by subclasses
+sub error{
+    return shift->{'error'} || undef;
 }
 
-sub stringify {
-  my ($self) = @_;
+# __internal__ #
 
-  my $string = $self->error;
+sub _new{
+    my ( $incovant, @args ) = @_;
 
-  unless ($self->only_error) {
-    $string .= ' at ' . $self->filename
-      if $self->has_filename;
+    my %params;
+    if ( @args == 1 && !ref $args[0] ) {
+	    %params = ('error' => $args[0]);
+    } else {
+        %params = ( @args );
+    }
 
-    $string .= ' line ' . $self->line
-      if $self->has_line;
+    my $class = ref( $incovant ) || $incovant;
+    my $self = bless( \%params, $class );
 
-    $string .= "\n";
-  }
-
-  return $string;
+#serious business
+    foreach my $key ( keys( %params ) ){
+        $self->_mk_accessor( $key );
+    }
+ 
+    return $self;
 }
 
-__PACKAGE__->meta->make_immutable;
+#creates an accessor for $name if it's not an existing method
+sub _mk_accessor{
+    my ( $self, $name ) = @_;
+
+    if ( !__PACKAGE__->can($name) ){
+    #create accessor if function doesn't exist
+        {
+            no strict 'refs';
+            *{"@{[ __PACKAGE__ ]}::${name}"} = sub {
+                return $self->{ $name } || undef;
+            };
+        }
+    }
+}
 
 1;
 
-__END__
-
-=pod
-
 =head1 NAME
 
-Exception::Simple - Exception classes and nothing more.
+Exception::Simple - simple exception class
 
 =head1 SYNOPSIS
 
-  use Exception::Simple;
-  use Scalar::Util qw/blessed/;
+    use Exception::Simple;
+    use Try::Tiny; #or just use eval {}, it's all good
 
-  eval {
-    throw Exception::Simple error => "Oh noes";
-  };
+    try{
+        Exception::Simple->throw( 'oh noes!' );
+    } catch {
+        warn $_; 
+        warn $_->error;
+    };
 
-  if (blessed $@ && $@->isa ('Exception::Simple')) {
-    # Do something.
-  }
+    my $data = { 
+        'foo' => 'bar',
+        'fibble' => [qw/wibble bibble/],
+    };
+    try{
+        Exception::Simple->throw( 
+            'error' => 'oh noes!',
+            'data' => $data,
+        );  
+    } catch {
+        warn $_; 
+        warn $_->error;
+
+        warn $_->data->{'foo'};
+    };
 
 =head1 DESCRIPTION
 
-Why another exception class? Well, as I was looking around CPAN for an
-exception class for my project, I found a lot of exception classes and
-none that suited my need; A simple exception class that didn't have
-any elaborate try/catch syntax, was easy to work with after exceptions
-were thrown, etc.
+pretty simple exception class. auto creates argument accessors.
 
-This module aims to do nothing but being an exception class. No sugar,
-no utility modules, just an exception class that should be flexible
-and easy to extend.
-
-=head1 ATTRIBUTES
-
-The module has a number of attributes that may be specified or even
-fills in automagically depending on how the exception is created. In
-addition to setting them as arguments to new/throw/rethrow, they may
-also be both retrieved and set through accessors. All optional
-attributes has a special has_attributename method that can be used to
-determine if they are set or not.
-
-=over 4
-
-=item error (mandatory)
-
-A mandatory description of the error being thrown.
-
-=item package (optional, automagic)
-
-Which package the exception was thrown in. Will be filled in
-automagically if not specified. This is currently not used for
-generating the error message but caller provides this so we store it.
-
-=item filename (optional, automagic)
-
-Which filename the code we throw in belongs to. Will as with package
-be filled in automagically if nothing is provided.
-
-=item line (optional, automagic)
-
-Which line we threw the exception at. Also automagic.
-
-=back
+simple, lightweight and extensible are this modules goals.
 
 =head1 METHODS
 
-=over 4
+=head2 throw
 
-=item throw
+    #with just one argument $@->error is set
+    Exception::Simple->throw( 'error message' );
+    # $@ stringifies to $@->error
 
-Throws an exception. It will collect information about the caller and
-fill in the attributes with this unless they are provided by the user.
-Then the exception object is created 
+    #or set multiple arguments (creates accessors)
+    Exception::Simple->throw( 
+        error => 'error message',
+        data => 'cutom atrribute',
+    );
+    # warn $@->data or something
 
-=item rethrow
+=head2 rethrow
 
-Rethrow throws an existing object. It will unlike throw not attempt to
-collect any information about the caller even if none currently is
-set.
+say you catch an error, but then you want to uncatch it
 
-=item new
+    use Try::Tiny;
 
-Simply creates the object. Does not collect any information about the
-environment, nor does it cause the exception to be thrown.
+    try{
+        Exception:Simple->throw( 'foobar' );
+    } catch {
+        if ( $_ eq 'foobar' ){
+        #not our error, rethrow
+            $_->rethrow; 
+        }
+    };
 
-=item stringify
+=head2 error
 
-Generates a string representation of the exception, similar to an
-ordinary die() message. The stringification overload calls this method
-to generate an error, so override this if you want to change that.
+accessor for error, if its been set
 
-=back
+=head1 SUPPORT
 
-=head1 CAVEATS
+Bugs should always be submitted via the CPAN bug tracker
 
-My philosophy is that once an exception is thrown, you're no longer
-interested in performance as top priority. This module uses L<Moose>
-beneath the hood, which is a slightly heavy module. This may change in
-the future, but will in any case not affect you when no exceptions are
-thrown.
+For other issues, contact the maintainer
+
+=head1 AUTHORS
+
+n0body E<lt>n0body@thisaintnews.comE<gt>
 
 =head1 SEE ALSO
 
-=over 4
+L<http://thisaintnews.com>, L<Try::Tiny>
 
-=item L<Exception::Class>
+=head1 LICENSE
 
-=item L<Moose>
+Copyright (C) 2011 by n0body L<http://thisaintnews.com/>
 
-=back
-
-=head1 BUGS
-
-Most software has bugs. This module probably isn't an exception. 
-If you find a bug please either email me, or add the bug to cpan-RT.
-
-=head1 AUTHOR
-
-Anders Nor Berle E<lt>debolaz@gmail.comE<gt>
-
-=head1 COPYRIGHT AND LICENSE
-
-Copyright 2007 by Anders Nor Berle.
-
-This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself. 
+This library is free software, you can redistribute it and/or modify
+it under the same terms as Perl itself.
 
 =cut
-
